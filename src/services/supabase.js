a7843ef0a3626a22bb86d90cbf0ee08f13b60953
shawky  sobhy <shawkysobhy98@gmail.com>
2024-02-26 11:22:15 +0200

    updata src

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = 'https://cerqqyruawwpxutetviu.supabase.co';
-const supabaseKey ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlcnFxeXJ1YXd3cHh1dGV0dml1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg3MDIxMzQsImV4cCI6MjAyNDI3ODEzNH0.L2DN6IanxcJ8PKJW51eTs_lI0Gy_6O1PJ__b2Tdgcls';
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, import.meta.env.VITE_SUPABASE_KEY);
 
 export default supabase;
